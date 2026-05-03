400ff8837a88eaefcf222f5ee88849f1d051cd3c
atalayd <104319149+atalayd@users.noreply.github.com>
2026-05-03 04:12:05 -0400

    chore: scaffold src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { diff, summarize, severityOf } from "./diff.js";
+export type {
+  DiffEntry,
+  DiffKind,
+  DiffOptions,
+  DiffSummary,
+} from "./diff.js";
+export { parsePayload } from "./parsers.js";
+export type { ParsedPayload, PayloadKind } from "./parsers.js";
+export { formatDiffs } from "./format.js";
+export type { FormatOptions } from "./format.js";
